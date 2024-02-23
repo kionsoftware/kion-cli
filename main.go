0fd8fdbdb6794c6b13c5b4ab81333c5db0ed6386
codybuell <cody@codybuell.com>
2024-02-23 15:25:28 -0500

    Bump version

--- main.go
+++ main.go
@@ -537,7 +537,7 @@ func main() {
 		////////////////
 
 		Name:                 "Kion CLI",
-		Version:              "v0.0.1",
+		Version:              "v0.0.2",
 		Usage:                "Kion federation on the command line!",
 		EnableBashCompletion: true,
 		Before:               beforeCommands,
