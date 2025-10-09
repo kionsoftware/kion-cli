e7ab25b1c3bd37faa993794ff34cfc12dca1ce9f
codybuell <cody@codybuell.com>
2025-10-09 13:41:36 -0400

    Configure region precedence for stak and run cmds
    flag > env vars > fav > config default > null

--- lib/commands/validators.go
+++ lib/commands/validators.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"errors"
 
+	"github.com/kionsoftware/kion-cli/lib/helper"
+	"github.com/kionsoftware/kion-cli/lib/structs"
 	"github.com/urfave/cli/v2"
 )
 
@@ -12,7 +14,7 @@ import (
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-// validateCmdStak validates the flags passed to the stak command.
+// ValidateCmdStak validates the flags passed to the stak command.
 func (c *Cmd) ValidateCmdStak(cCtx *cli.Context) error {
 	if (cCtx.String("account") != "" || cCtx.String("alias") != "") && cCtx.String("car") == "" {
 		return errors.New("must specify --car parameter when using --account or --alias")
@@ -22,7 +24,7 @@ func (c *Cmd) ValidateCmdStak(cCtx *cli.Context) error {
 	return nil
 }
 
-// validateCmdConsole validates the flags passed to the console command.
+// ValidateCmdConsole validates the flags passed to the console command.
 func (c *Cmd) ValidateCmdConsole(cCtx *cli.Context) error {
 	if cCtx.String("car") != "" {
 		if cCtx.String("account") == "" && cCtx.String("alias") == "" {
@@ -34,10 +36,27 @@ func (c *Cmd) ValidateCmdConsole(cCtx *cli.Context) error {
 	return nil
 }
 
-// validateCmdRun validates the flags passed to the run command.
+// ValidateCmdRun validates the flags passed to the run command and sets the
+// favorites region as the default region if needed to ensure precedence.
 func (c *Cmd) ValidateCmdRun(cCtx *cli.Context) error {
+	// Validate that either a favorite is used or both account/alias and car are provided
 	if cCtx.String("favorite") == "" && ((cCtx.String("account") == "" && cCtx.String("alias") == "") || cCtx.String("car") == "") {
 		return errors.New("must specify either --fav OR --account and --car  OR --alias and --car parameters")
 	}
+
+	// Set the favorite region as the default region if a favorite is used
+	favName := cCtx.String("favorite")
+	_, fMap := helper.MapFavs(c.config.Favorites)
+	var fav string
+	if fMap[favName] != (structs.Favorite{}) {
+		fav = favName
+	} else {
+		return errors.New("can't find favorite")
+	}
+	favorite := fMap[fav]
+	if favorite.Region != "" {
+		c.config.Kion.DefaultRegion = favorite.Region
+	}
+
 	return nil
 }
