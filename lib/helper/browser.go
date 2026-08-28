8b8f5be023aa280a51c75454e81fd1cd27387d67
prehr <43080283+prehr@users.noreply.github.com>
2026-08-28 13:34:43 -0400

    aws multi-session removing unnecessary fmt.Sprintf

--- lib/helper/browser.go
+++ lib/helper/browser.go
@@ -253,7 +253,7 @@ func OpenBrowserRedirect(target string, session structs.SessionInfo, config stru
 	} else {
 		federationLink = fmt.Sprintf("%s%s", logoutURL, encodedUrlRedirect)
 		if session.AWSMultiSession && session.AccountTypeID == 1 {
-			federationLink = fmt.Sprintf("%s", redirectTarget)
+			federationLink = redirectTarget
 		}
 	}
 	// open the browser
